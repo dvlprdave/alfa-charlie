84b79fc6dc8a9f66d268e789e323840e25588e6c
David <devdavee@gmail.com>
2020-07-07 19:31:11 -0400

    Initialize project using Create React App

---
+++ src/theme/theme.js
@@ -0,0 +1,14 @@
+// You can store all of your styles here to be used within the theme object and pass them as props to your CSS declarations
+
+/* 
+  Example:
+    const ExampleDiv = styled.div`
+      colors: ${props => props.theme.colors.white};   
+    `
+*/
+
+export default {
+  colors: {
+    white: '#ffff'
+  }
+}
